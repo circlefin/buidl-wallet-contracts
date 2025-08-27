ce0c4e6c1324ce9139da2252b50c63d0e767d41a
Hieu Dang <hieu.dang@circle.com>
2025-08-27 15:33:22 -0700

    deploy testnet plugins and factories

--- script/bytecode-deploy/100_Constants.sol
+++ script/bytecode-deploy/100_Constants.sol
@@ -32,32 +32,15 @@ address constant SPONSOR_PAYMASTER_INTERNAL_ADDRESS = 0x67aC303faB3A9507Ac48fD5B
 address constant SPONSOR_PAYMASTER_ADDRESS = 0x7ceA357B5AC0639F89F9e378a1f03Aa5005C0a25;
 
 library Constants {
-    function getChains() internal pure returns (string[18] memory) {
+    function getChains() internal pure returns (string[1] memory) {
         return [
-            "mainnet",
-            "sepolia",
-            "polygon",
-            "amoy",
-            "arbitrum",
-            "arb-sepolia",
-            "uni-sepolia",
-            "unichain",
-            "base-sepolia",
-            "base",
-            "op-sepolia",
-            "op",
-            "sonic",
-            "sonic-blaze",
-            "world",
-            "world-sepolia",
-            "celo",
-            "celo-alfajores"
+            "sei-testnet"
         ];
     }
 
     // A list of chains that need to run setup work after deployment. Please adjust the list based on the chain
     // expansion needs.
-    function getCurrentDeploymentChains() internal pure returns (string[3] memory) {
-        return ["sonic", "world", "celo"];
+    function getCurrentDeploymentChains() internal pure returns (string[1] memory) {
+        return ["sei-testnet"];
     }
 }
