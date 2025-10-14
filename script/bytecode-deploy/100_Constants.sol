1a7217da977b65ed9ffa644a03154d3eeb5653a2
Hieu Dang <hieu.dang@circle.com>
2025-10-13 18:46:46 -0700

    update number of chains

--- script/bytecode-deploy/100_Constants.sol
+++ script/bytecode-deploy/100_Constants.sol
@@ -28,7 +28,7 @@ address constant COLD_STORAGE_ADDRESS_BOOK_PLUGIN_ADDRESS = 0x0000000d81083B16EA
 address constant WEIGHTED_MULTISIG_PLUGIN_ADDRESS = 0x0000000C984AFf541D6cE86Bb697e68ec57873C8;
 
 library Constants {
-    function getChains() internal pure returns (string[1] memory) {
+    function getChains() internal pure returns (string[17] memory) {
         return [
             "mainnet",
             "sepolia",
