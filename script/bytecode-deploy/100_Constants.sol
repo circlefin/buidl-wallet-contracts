53d281db21d4e64afaa1cd75dbbd789b64275946
Hieu Dang <hieu.dang@circle.com>
2025-12-05 12:30:07 -0800

    feat: Deploy Arc Testnet, Monad Testnet and Monad paymaster and v2, v3 factory contracts (#101)
    ## Summary
    
    Contracts are deployed and verified:
    
    **Monad**
    [Plugin
    Manager](https://monadscan.com/address/0xc93D6559Fe4dB59742751A857d11a04861a50CCC)
    
    [SingleOwnerMSCAFactory](https://monadscan.com/address/0xf61023061ed45fa9eAC4D2670649cE1FD37ce536)
    
    [ColdStorageAddressBookPlugin](https://monadscan.com/address/0x11BACb0F0C9D04A28A2043b54c0C3C2CE15F0Af4)
    [EPV0.6 Paymaster Proxy
    STG](https://monadscan.com/address/0x67aC303faB3A9507Ac48fD5B5EE176c4E39aE98C)
    [EPV0.6 Paymaster Proxy
    PROD](https://monadscan.com/address/0x7ceA357B5AC0639F89F9e378a1f03Aa5005C0a25)
    [EPV0.6 Paymaster Implementation
    
    ](https://monadscan.com/address/0x26f77C6455518CAF29A5414004C5A2Ef441228b1)
    
    [Plugin manager
    v2](https://monadscan.com/address/0x3169ad878021b87c9caa9b5cda740ff3ca270ce9)
    [SingleOwnerMSCAFactory
    v2](https://monadscan.com/address/0xa233b124d7b9cff2d38cb62319e1a3f79144b490)
    
    **Monad Testnet**
    [Plugin
    Manager](https://testnet.monadscan.com/address/0xc93D6559Fe4dB59742751A857d11a04861a50CCC)
    
    [SingleOwnerMSCAFactory](https://testnet.monadscan.com/address/0xf61023061ed45fa9eAC4D2670649cE1FD37ce536)
    
    [ColdStorageAddressBookPlugin](https://testnet.monadscan.com/address/0x11BACb0F0C9D04A28A2043b54c0C3C2CE15F0Af4)
    [EPV0.6 Paymaster Proxy
    STG](https://testnet.monadscan.com/address/0x67aC303faB3A9507Ac48fD5B5EE176c4E39aE98C)
    [EPV0.6 Paymaster Proxy
    PROD](https://testnet.monadscan.com/address/0x7ceA357B5AC0639F89F9e378a1f03Aa5005C0a25)
    [EPV0.6 Paymaster Implementation
    
    ](https://testnet.monadscan.com/address/0x26f77C6455518CAF29A5414004C5A2Ef441228b1)
    
    [Plugin manager
    v2](https://testnet.monadscan.com/address/0x3169ad878021b87c9caa9b5cda740ff3ca270ce9)
    [SingleOwnerMSCAFactory
    v2](https://testnet.monadscan.com/address/0xa233b124d7b9cff2d38cb62319e1a3f79144b490)
    
    **Arc Testnet**
    [Plugin
    Manager](https://testnet.arcscan.app/address/0xc93D6559Fe4dB59742751A857d11a04861a50CCC)
    
    [SingleOwnerMSCAFactory](https://testnet.arcscan.app/address/0xf61023061ed45fa9eAC4D2670649cE1FD37ce536)
    
    [ColdStorageAddressBookPlugin](https://testnet.arcscan.app/address/0x11BACb0F0C9D04A28A2043b54c0C3C2CE15F0Af4)
    [EPV0.6 Paymaster Proxy
    STG](https://testnet.arcscan.app/address/0x67aC303faB3A9507Ac48fD5B5EE176c4E39aE98C)
    [EPV0.6 Paymaster Proxy
    PROD](https://testnet.arcscan.app/address/0x7ceA357B5AC0639F89F9e378a1f03Aa5005C0a25)
    [EPV0.6 Paymaster Implementation
    
    ](https://testnet.arcscan.app/address/0x26f77C6455518CAF29A5414004C5A2Ef441228b1)
    
    [Plugin manager
    v2](https://testnet.arcscan.app/address/0x3169ad878021b87c9caa9b5cda740ff3ca270ce9)
    [SingleOwnerMSCAFactory
    v2](https://testnet.arcscan.app/address/0xa233b124d7b9cff2d38cb62319e1a3f79144b490)
    
    ## Detail
    Contracts are deployed for Monad, Monad Testnet, and Arc Testnet for
    epv0.6. All contracts are verified
    
    ### Changeset
    N/A
    
    ### Checklist
    - [ ] Did you add new tests and confirm all tests pass? (`yarn test`)
    - [ ] Did you ensure any new Solidity source code files meet minimum
    test coverage requirements? (`yarn coverage`)
    - [ ] Did you update relevant docs? (docs are found in the `docs`
    folder)
    - [ ] Do your commits follow the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) standard?
    - [ ] Does your PR title also follow the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) standard?
    - [ ] Did you run lint (`yarn lint`) and fix any issues?
    - [ ] Did you run formatter (`yarn format:check`) and fix any issues
    (`yarn format:write`)?
    
    ## Testing
    Tests passed
    
    ## Documentation
    N/A

--- script/bytecode-deploy/100_Constants.sol
+++ script/bytecode-deploy/100_Constants.sol
@@ -32,7 +32,7 @@ address constant SPONSOR_PAYMASTER_INTERNAL_ADDRESS = 0x67aC303faB3A9507Ac48fD5B
 address constant SPONSOR_PAYMASTER_ADDRESS = 0x7ceA357B5AC0639F89F9e378a1f03Aa5005C0a25;
 
 library Constants {
-    function getChains() internal pure returns (string[20] memory) {
+    function getChains() internal pure returns (string[23] memory) {
         return [
             "mainnet",
             "sepolia",
@@ -53,13 +53,16 @@ library Constants {
             "celo",
             "celo-alfajores",
             "sei",
-            "sei-testnet"
+            "sei-testnet",
+            "arc-testnet",
+            "monad-testnet",
+            "monad"
         ];
     }
 
     // A list of chains that need to run setup work after deployment. Please adjust the list based on the chain
     // expansion needs.
     function getCurrentDeploymentChains() internal pure returns (string[1] memory) {
-        return ["sei"];
+        return ["monad"];
     }
 }
