7b2c231dcd5787645af045f829fa73fbed47facf
hieu-dang_crcl <hieu.dang@circle.com>
2025-12-30 14:32:01 -0800

    deploy ep0.6 Edge paymasters, plugin manager, factory, plugin contracts

--- script/bytecode-deploy/100_Constants.sol
+++ script/bytecode-deploy/100_Constants.sol
@@ -32,7 +32,7 @@ address constant SPONSOR_PAYMASTER_INTERNAL_ADDRESS = 0x67aC303faB3A9507Ac48fD5B
 address constant SPONSOR_PAYMASTER_ADDRESS = 0x7ceA357B5AC0639F89F9e378a1f03Aa5005C0a25;
 
 library Constants {
-    function getChains() internal pure returns (string[23] memory) {
+    function getChains() internal pure returns (string[25] memory) {
         return [
             "mainnet",
             "sepolia",
@@ -56,13 +56,15 @@ library Constants {
             "sei-testnet",
             "arc-testnet",
             "monad-testnet",
-            "monad"
+            "monad",
+            "edge-testnet",
+            "edge"
         ];
     }
 
     // A list of chains that need to run setup work after deployment. Please adjust the list based on the chain
     // expansion needs.
     function getCurrentDeploymentChains() internal pure returns (string[1] memory) {
-        return ["monad"];
+        return ["edge"];
     }
 }
